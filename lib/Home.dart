bbfe1d3817c0bfd8de6f3e0864b24e55d55bad18
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-05 17:53:11 -0400

    Criado o visual e BottomBar.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Inicio.dart';
+import 'package:youtube/telas/Inscricao.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +12,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca()
+
+    ];
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
@@ -42,7 +58,39 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice){
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.orange,
+            icon: Icon(Icons.home),
+            label: "Início",
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.red,
+            icon: Icon(Icons.whatshot),
+            label: "Em alta",
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.blue,
+            icon: Icon(Icons.subscriptions),
+            label: "Inscrições",
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Colors.green,
+            icon: Icon(Icons.folder),
+            label: "Biblioteca",
+          ),
+        ],
+      ),
     );
   }
 }
