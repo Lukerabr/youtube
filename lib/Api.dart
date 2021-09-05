3c3a835cc0f28b8143a2ca69ebc3dc8325ffd729
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-05 19:09:53 -0400

    Convertendo o retorno da API.

--- lib/Api.dart
+++ lib/Api.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'model/Video.dart';
+
 const CHAVE_YOUTUBE_API = "AIzaSyBNBps6bSQ7QpDVjWAjYFYQkn--F8KETno";
 const ID_CANAL = "UCW0gH2G-cMKAEjEkI4YhnPA";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
@@ -24,7 +26,24 @@ class Api {
     if(response.statusCode == 200){
 
       Map<String, dynamic> dadosJson = json.decode(response.body);
-      print("Resultado: " + dadosJson["items"][4]["snippet"]["title"].toString());
+
+      List<Video> videos = dadosJson["items"].map<Video>(
+          (map){
+            return Video.fromJson(map);
+          }
+      ).toList();
+
+      for(var video in videos){
+        print("resultado: " + video.titulo.toString()); // colocar o toString pois foi iniciado o parâmetro como String?
+      }
+
+      /*
+      for(var video in dadosJson["items"]){
+        print("Resultado: " + video.toString());
+      }
+      */
+
+
 
     }else{
 
--- lib/model/Video.dart
+++ lib/model/Video.dart
@@ -8,5 +8,25 @@ class Video {
 
   Video({this.id, this.titulo, this.descricao, this.imagem, this.canal});
 
+  /* Resolve mas não otimiza o problema
+  static converterJson(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"]
+    );
+  }
+  */
+  //Cria uma unica instancia do objeto e retorna
+  factory Video.fromJson(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"]
+  );
+  }
+
 
 }
